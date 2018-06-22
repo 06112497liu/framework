30fc5c042125c2096b9f5145b464891560f71e3a
06112497liu <804912547@qq.com>
2018-06-22 19:05:54 +0800

    拦截器

--- system-web/src/main/java/com/lwb/config/WebAppConfig.java
+++ system-web/src/main/java/com/lwb/config/WebAppConfig.java
@@ -3,8 +3,13 @@ package com.lwb.config;
 import com.lwb.interceptor.TokenInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.nio.charset.Charset;
+import java.util.List;
 
 /**
  * @author liuweibo
@@ -25,5 +30,17 @@ public class WebAppConfig extends WebMvcConfigurerAdapter {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(tokenInterceptor())
             .addPathPatterns("/**");
+    }
+
+    @Bean
+    public HttpMessageConverter<String> responseBodyConverter() {
+        StringHttpMessageConverter converter = new StringHttpMessageConverter(Charset.forName("UTF-8"));
+        return converter;
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        super.configureMessageConverters(converters);
+        converters.add(responseBodyConverter());
     }
 }
